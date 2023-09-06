e5f2f0004621dddbbb21edcaf797e25fb6196ad0
harshithaachary <harshithaacharya26@gmail.com>
2023-09-06 17:06:08 +0530

    trial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Greeting from './Greeting';
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to my App!</h1>
+      <Greeting name="John" />
+    </div>
+  );
+};
+export default App;
